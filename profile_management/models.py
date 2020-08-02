4384136ad022a2b91eeddf7059c6036c9cc2e50b
prakashgun <mail.prakash.india@gmail.com>
2020-08-02 14:29:40 +0530

    Matrimony models created

--- profile_management/models.py
+++ profile_management/models.py
@@ -1,3 +1,297 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class MaritalStatus(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class PhysicalStatus(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class BodyType(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Complexion(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class CreatedBy(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class EatingHabit(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class DrinkingHabit(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class SmokingHabit(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Religion(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Caste(models.Model):
+    name = models.CharField(max_length=255)
+    religion = models.ForeignKey(Religion, on_delete=models.CASCADE)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Star(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Raasi(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Dosham(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Education(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Occupation(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class EmployedIn(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class CurrencyType(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class FamilyValue(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class FamilyType(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class FamilyStatus(models.Model):
+    name = models.CharField(max_length=255)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    """All information about user other than authentication"""
+    objects = models.Manager()
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    dob = models.DateField()
+    about = models.TextField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    marital_status = models.ForeignKey(
+        MaritalStatus, on_delete=models.CASCADE,
+        related_name='marital_status_profile_set')
+    mother_tongue = models.ForeignKey(
+        Language, on_delete=models.CASCADE,
+        related_name='mother_tongue_profile_set')
+    physical_status = models.ForeignKey(
+        PhysicalStatus,
+        on_delete=models.CASCADE,
+        related_name='physical_status_profile_set')
+    created_by = models.ForeignKey(
+        CreatedBy, on_delete=models.CASCADE,
+        related_name='created_by_profile_set')
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE,
+        related_name='country_profile_set')
+    state = models.ForeignKey(
+        State, on_delete=models.CASCADE,
+        related_name='state_profile_set')
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE,
+        related_name='city_profile_set')
+    citizenship = models.ForeignKey(
+        Country, on_delete=models.CASCADE,
+        related_name='citizenship_profile_set')
+    eating_habit = models.ForeignKey(
+        EatingHabit, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='eating_habit_profile_set')
+    drinking_habit = models.ForeignKey(
+        DrinkingHabit, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='drinking_habit_profile_set')
+    smoking_habit = models.ForeignKey(
+        SmokingHabit, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='smoking_habit_profile_set')
+    education = models.ForeignKey(
+        Education, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='education_profile_set')
+    education_institution = models.CharField(max_length=255, blank=True,
+                                             null=True)
+    occupation = models.ForeignKey(
+        Occupation, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='occupation_profile_set')
+    occupation_in_detail = models.CharField(max_length=255, blank=True,
+                                            null=True)
+    organization = models.CharField(max_length=255, blank=True, null=True)
+    employed_in = models.ForeignKey(
+        EmployedIn, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='employed_in_profile_set')
+    currency_type = models.ForeignKey(
+        CurrencyType, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='currency_type_profile_set')
+    annual_income = models.IntegerField(blank=True, null=True)
+    family_value = models.ForeignKey(
+        FamilyValue, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='family_value_profile_set')
+    family_type = models.ForeignKey(
+        FamilyType, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='family_type_profile_set')
+    family_status = models.ForeignKey(
+        FamilyStatus, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='family_status_profile_set')
+    father_occupation = models.ForeignKey(
+        Occupation, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='father_occupation_profile_set')
+    mother_occupation = models.ForeignKey(
+        Occupation, on_delete=models.CASCADE,
+        blank=True, null=True,
+        related_name='mother_occupation_profile_set')
+    no_of_brothers = models.IntegerField(blank=True, null=True)
+    brothers_married = models.IntegerField(blank=True, null=True)
+    no_of_sisters = models.IntegerField(blank=True, null=True)
+    sisters_married = models.IntegerField(blank=True, null=True)
+    ancestral_origin = models.CharField(max_length=255, blank=True, null=True)
+    about_family = models.TextField(blank=True, null=True)
+    spoken_languages = models.ManyToManyField(
+        Language, blank=True, related_name='spoken_languages_profile_set')
+
+    def __str__(self):
+        return self.about
--- profile_management/tests.py
+++ profile_management/tests.py
@@ -1,3 +1,207 @@
+from datetime import datetime
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from . import models
+
+
+class ModelTests(TestCase):
+
+    def setUp(self):
+        self.marital_status = models.MaritalStatus.objects.create(
+            name="Never Married"
+        )
+        self.language = models.Language.objects.create(
+            name="Malayalam"
+        )
+        self.language2 = models.Language.objects.create(
+            name="English"
+        )
+        self.physical_status = models.PhysicalStatus.objects.create(
+            name="Normal"
+        )
+        self.body_type = models.BodyType.objects.create(
+            name="Slim"
+        )
+        self.complexion = models.Complexion.objects.create(
+            name="Fair"
+        )
+        self.created_by = models.CreatedBy.objects.create(
+            name="Self"
+        )
+        self.eating_habit = models.EatingHabit.objects.create(
+            name="Vegetarian"
+        )
+        self.drinking_habit = models.DrinkingHabit.objects.create(
+            name="Non Drinker"
+        )
+        self.smoking_habit = models.SmokingHabit.objects.create(
+            name="Non Smoker"
+        )
+        self.religion = models.Religion.objects.create(
+            name="Hindu"
+        )
+        self.caste = models.Caste.objects.create(
+            religion=self.religion,
+            name="Ezhava"
+        )
+        self.star = models.Star.objects.create(
+            name="Aswathi"
+        )
+        self.raasi = models.Raasi.objects.create(
+            name="Medam"
+        )
+        self.dosham = models.Dosham.objects.create(
+            name="No"
+        )
+        self.education = models.Education.objects.create(
+            name="BE"
+        )
+        self.occupation = models.Occupation.objects.create(
+            name="Software Professional"
+        )
+        self.employed_in = models.EmployedIn.objects.create(
+            name="Private"
+        )
+        self.currency_type = models.CurrencyType.objects.create(
+            name="Rs."
+        )
+        self.country = models.Country.objects.create(
+            name="India"
+        )
+        self.state = models.State.objects.create(
+            country=self.country,
+            name="Tamil Nadu"
+        )
+        self.city = models.City.objects.create(
+            state=self.state,
+            name="Coimbatore"
+        )
+        self.family_value = models.FamilyValue.objects.create(
+            name="Liberal"
+        )
+        self.family_type = models.FamilyType.objects.create(
+            name="Nuclear Family"
+        )
+        self.family_status = models.FamilyStatus.objects.create(
+            name="Middle Class"
+        )
+
+    def test_marital_status_str(self):
+        self.assertEqual(str(self.marital_status), self.marital_status.name)
+
+    def test_language_str(self):
+        self.assertEqual(str(self.language), self.language.name)
+
+    def test_physical_status_str(self):
+        self.assertEqual(str(self.physical_status), self.physical_status.name)
+
+    def test_body_type_str(self):
+        self.assertEqual(str(self.body_type), self.body_type.name)
+
+    def test_complexion_str(self):
+        self.assertEqual(str(self.complexion), self.complexion.name)
+
+    def test_created_by_str(self):
+        self.assertEqual(str(self.created_by), self.created_by.name)
+
+    def test_eating_habit_str(self):
+        self.assertEqual(str(self.eating_habit), self.eating_habit.name)
+
+    def test_drinking_habit_str(self):
+        self.assertEqual(str(self.drinking_habit), self.drinking_habit.name)
+
+    def test_smoking_habit_str(self):
+        self.assertEqual(str(self.smoking_habit), self.smoking_habit.name)
+
+    def test_religion_str(self):
+        self.assertEqual(str(self.religion), self.religion.name)
+
+    def test_caste_str(self):
+        self.assertEqual(str(self.caste), self.caste.name)
+
+    def test_star_str(self):
+        self.assertEqual(str(self.star), self.star.name)
+
+    def test_raasi_str(self):
+        self.assertEqual(str(self.raasi), self.raasi.name)
+
+    def test_dosham_str(self):
+        self.assertEqual(str(self.dosham), self.dosham.name)
+
+    def test_education_str(self):
+        self.assertEqual(str(self.education), self.education.name)
+
+    def test_occupation_str(self):
+        self.assertEqual(str(self.occupation), self.occupation.name)
+
+    def test_employed_in_str(self):
+        self.assertEqual(str(self.employed_in), self.employed_in.name)
+
+    def test_currency_type_str(self):
+        self.assertEqual(str(self.currency_type), self.currency_type.name)
+
+    def test_country_str(self):
+        self.assertEqual(str(self.country), self.country.name)
+
+    def test_state_str(self):
+        self.assertEqual(str(self.state), self.state.name)
+
+    def test_city_str(self):
+        self.assertEqual(str(self.city), self.city.name)
+
+    def test_family_value_str(self):
+        self.assertEqual(str(self.family_value), self.family_value.name)
+
+    def test_family_type_str(self):
+        self.assertEqual(str(self.family_type), self.family_type.name)
+
+    def test_family_status_str(self):
+        self.assertEqual(str(self.family_status), self.family_status.name)
+
+    def test_profile_str(self):
+        profile = models.Profile.objects.create(
+            user=get_user_model().objects.create(
+                username='9876543210', password='test',
+                email='test@example.com'
+            ),
+            dob=datetime.strptime('1995-08-11', '%Y-%m-%d').date(),
+            about='Software Engineer working in Bangalore',
+            height=182,
+            weight=70,
+            marital_status=self.marital_status,
+            mother_tongue=self.language,
+            physical_status=self.physical_status,
+            created_by=self.created_by,
+            eating_habit=self.eating_habit,
+            drinking_habit=self.drinking_habit,
+            smoking_habit=self.smoking_habit,
+            education=self.education,
+            education_institution='CMS College of Science and Commerce',
+            occupation=self.occupation,
+            occupation_in_detail='Software Programmer',
+            organization='Spi Global',
+            employed_in=self.employed_in,
+            currency_type=self.currency_type,
+            annual_income=10,
+            country=self.country,
+            state=self.state,
+            city=self.city,
+            citizenship=self.country,
+            family_value=self.family_value,
+            family_type=self.family_type,
+            family_status=self.family_status,
+            father_occupation=self.occupation,
+            mother_occupation=self.occupation,
+            no_of_brothers=1,
+            brothers_married=None,
+            no_of_sisters=1,
+            sisters_married=None,
+            ancestral_origin='Parsi',
+            about_family='Middle class family staying in Bangalore'
+        )
+
+        profile.spoken_languages.set([self.language, self.language2])
+
+        self.assertEqual(str(profile), profile.about)
